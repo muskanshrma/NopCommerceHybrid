10cad4ce547eebfe3e7e2b8e4ef8fac233df86e0
muskanshrma <muskan.sharma@infostride.com>
2022-10-11 05:55:50 -0700

    Hybrid Framework

---
+++ src/test/java/Pages/PageFactory.java
@@ -0,0 +1,12 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageFactory {
+    WebDriver driver;
+
+    public PageFactory(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
